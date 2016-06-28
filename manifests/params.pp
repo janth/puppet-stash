48c932f9f070073e4248fe34c5437da93c8736db
Jan Thomas Moldung <jan.thomas.moldung@evry.com>
2016-06-28 17:04:16 +0200

    Change hardcoded ...-stash to ...-$product, removed more hardcoding take 3

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,22 +11,22 @@ class stash::params {
         # https://wiki.archlinux.org/index.php/systemd#Writing_unit_files
         # /usr/lib/systemd/system/: units provided by installed packages
         # /etc/systemd/system/: units installed by the system administrator
-        $service_file_location   = "/usr/lib/systemd/system/$product.service"
+        $service_file_location   = "/usr/lib/systemd/system/$stash::product.service"
         $service_file_template   = 'stash/stash.service.erb'
-        $service_lockfile        = "/var/lock/subsys/$product"
+        $service_lockfile        = "/var/lock/subsys/$stash::product"
       } elsif $::operatingsystemmajrelease == '6' {
         $json_packages           = [ 'rubygem-json', 'ruby-json' ]
-        $service_file_location   = "/etc/init.d/$product"
+        $service_file_location   = "/etc/init.d/$stash::product"
         $service_file_template   = 'stash/stash.initscript.redhat.erb'
-        $service_lockfile        = "/var/lock/subsys/$product"
+        $service_lockfile        = "/var/lock/subsys/$stash::product"
       } else {
         fail("${::operatingsystem} ${::operatingsystemmajrelease} not supported")
       }
     } /Debian/: {
       $json_packages           = [ 'rubygem-json', 'ruby-json' ]
-      $service_file_location   = "/etc/init.d/$product"
+      $service_file_location   = "/etc/init.d/$stash::product"
       $service_file_template   = 'stash/stash.initscript.debian.erb'
-      $service_lockfile        = "/var/lock/$product"
+      $service_lockfile        = "/var/lock/$stash::product"
     } default: {
       fail("${::operatingsystem} ${::operatingsystemmajrelease} not supported")
     }
